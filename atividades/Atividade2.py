cc9b4cab2080aa52f4379272491734704be3ee10
Marcelo <marcelo.ferbat@gmail.com>
2026-09-01 21:49:06 -0300

    Correção booleano

--- atividades/Atividade2.py
+++ atividades/Atividade2.py
@@ -8,5 +8,5 @@ Exemplo de retorno: Seu nome é João, você tem 22 anos, Tem plano? False. Voc�
 #RESPOSTA
 nome = input("Insira seu nome: ")
 idade = int(input("Insira sua idade: "))
-possue_plano = str(input("Tem plano de saúde? "))
-print("Seu nome é",nome,"você tem", idade,"anos. Tem plano?",possue_plano,"Você foi aceito?", (idade < 18 or idade > 65 or possue_plano == False))
+possue_plano = bool(input("Tem plano de saúde? "))
+print("Seu nome é",nome,"você tem", idade,"anos. Tem plano?",possue_plano,"Você foi aceito?", (idade > 18 or idade < 65 or possue_plano == False))
