0cd9306fbe8244b3e5ff7a89e240f8f2dfa87062
marcelo <marcelo.ferbat@gmail.com>
2026-09-14 16:16:06 -0300

    correção função ativ8

--- atividades/Atividade8/Atividade8.py
+++ atividades/Atividade8/Atividade8.py
@@ -10,17 +10,20 @@ OBS: valor da média = 7
 """
 
 def aluno(nome, nota1, nota2, nota3, nota4):
-    nome = input("Digite o nome do aluno: ")
-    nota1 = float(input("Digite a primeira nota: "))
-    nota2 = float(input("Digite a segunda nota: "))
-    nota3 = float(input("Digite a terceira nota: "))
-    nota4 = float(input("Digite a quarta nota: "))
     media = (nota1 + nota2 + nota3 + nota4) / 4
     if media >= 7:
-        print("O aluno foi aprovado!")
+        situacao = "aprovado"
     else:
-        print("O aluno foi reprovado!")
-    return
+        situacao = "reprovado"
+    print(f"Aluno: {nome}")
+    print(f"Notas: {nota1}, {nota2}, {nota3}, {nota4}")
+    print(f"Média final: {media}")
+    print(f"O aluno foi {situacao}!")
 
+nome = input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
 
 aluno(nome, nota1, nota2, nota3, nota4)
