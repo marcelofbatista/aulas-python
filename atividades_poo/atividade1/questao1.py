3409b23d622f904776d1efa3bb01521d1309079d
Marcelo <marcelo.ferbat@gmail.com>
2026-09-15 21:46:51 -0300

    atividade poo questão1

---
+++ atividades_poo/atividade1/questao1.py
@@ -0,0 +1,28 @@
+"""
+Crie uma classe que tenha no mínimo 5 atributos, 1 construtor,
+3 métodos convencionais.
+Sua classe deve ser uma das opções abaixo:
+carro
+banco
+pessoa
+Você escolhe quais atributos relacionar com o conceito da sua
+classe.
+No final, quero 5 objetos diferentes instanciados, e seu
+programa deve exibir em uma lista fora da classe
+todos os seus objetos.
+"""
+
+class Pessoa:
+    def __init__(self, nome, idade, sexo, curso, turno):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.curso = curso
+        self.turno = turno
+
+
+pessoa1 = Pessoa("João", 18, "Masculino", "Python", "Noturno")
+pessoa2 = Pessoa("Tião", 30, "Masculino", "Java", "Vespertino")
+pessoa3 = Pessoa("Maria", 42, "Feminino", "C++", "Matutino")
+pessoa4 = Pessoa("Carlos", 54, "Masculino", "Fortran", "Noturno")
+pessoa5 = Pessoa("Clara", 16, "Feminino", "Redes Sociais", "Vespertino")
