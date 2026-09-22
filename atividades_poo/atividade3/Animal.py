1b848e0567d0a33bc663b525c9a09361eac3d179
marcelo <marcelo.ferbat@gmail.com>
2026-09-22 18:36:23 -0300

    exercício atividade 3

--- atividades_poo/atividade3/Animal.py
+++ atividades_poo/atividade3/Animal.py
@@ -1,8 +1,9 @@
 class Animal:
     def __init__(self, nome, idade, nivel_fome):
         self.__nome = nome
-        self.__idade = idade
-        self.__nivel_fome = nivel_fome
+        self.__idade = 0
+        self.idade = idade
+        self.nivel_fome = nivel_fome
 
     @property
     def nome(self):
@@ -16,10 +17,6 @@ class Animal:
     def nivel_fome(self):
         return self.__nivel_fome
 
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
     @idade.setter
     def idade(self, idade):
         if idade < 0:
@@ -29,13 +26,21 @@ class Animal:
 
     @nivel_fome.setter
     def nivel_fome(self, nivel_fome):
-        if nivel_fome < 0 or nivel_fome > 100:
-            print("Erro: Nível de fome deve estar entre 0 e 100")
-        else:
-            self.__nivel_fome = nivel_fome
+        if nivel_fome < 0:
+            nivel_fome = 0
+        elif nivel_fome > 100:
+            nivel_fome = 100
+        self.__nivel_fome = nivel_fome
 
     def alimentar(self,porcao):
-        if porcao < 0:
+        if porcao <= 0:
             print("Erro: Porção inválida")
         else:
-            self.__nivel_fome = porcao
+            self.nivel_fome = self.nivel_fome - porcao
+
+
+    def emitir_som(self):
+        print(f"{self.nome} faz um som genérico.")
+
+    def exibir_resumo(self):
+        print(f"Nome: {self.nome} | Idade: {self.idade} | Fome: {self.nivel_fome}")
--- atividades_poo/atividade3/Mamifero.py
+++ atividades_poo/atividade3/Mamifero.py
@@ -13,3 +13,14 @@ class Mamifero(Animal):
     @velocidade_kmh.setter
     def velocidade_kmh(self, velocidade_kmh):
         self.__velocidade_kmh = velocidade_kmh
+
+    def correr(self):
+        print(f"{self.nome} correu a {self.velocidade_kmh} km/h!")
+        self.nivel_fome = self.nivel_fome + 20
+
+    def emitir_som(self):
+        print(f"{self.nome} ruge alto!")
+
+    def exibir_resumo(self):
+        super().exibir_resumo()
+        print(f"Velocidade: {self.velocidade_kmh} km/h")
